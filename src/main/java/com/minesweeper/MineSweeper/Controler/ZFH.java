d38f6d3200b7afaa4b121c3acc7c2bc3cfba4ba0
YuchiYixuan <83524799+YuchiYixuan@users.noreply.github.com>
2021-05-02 17:02:54 +0800

    Delete ZFH.java

--- src/main/java/com/minesweeper/MineSweeper/Controler/ZFH.java
+++
@@ -1,8 +0,0 @@
-package com.minesweeper.MineSweeper.Controler;
-
-public class ZFH {
-    public static void main(String[] args) {
-        System.out.println("zfh");
-        System.out.println("yc YiXuan");
-    }
-}
