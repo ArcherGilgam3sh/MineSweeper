6a3f0959fa644164bdfdb5b4bae363dccedcb767
ArcherGilgam3sh <83524709+ArcherGilgam3sh@users.noreply.github.com>
2021-05-02 17:07:49 +0800

    Create HelloYuchiYixuan.java

---
+++ src/main/java/com/minesweeper/MineSweeper/HelloYuchiYixuan.java
@@ -0,0 +1,7 @@
+package com.minesweeper.MineSweeper;
+
+public class HelloYuchiYixuan {
+    public static void main(String[] args) {
+        System.out.println("YuchiYixuan is really stupid.");
+    }
+}
