8760bb8e63021fedf2fbb56d7ecbc35d412ec2cc
ArcherGilgam3sh <83524709+ArcherGilgam3sh@users.noreply.github.com>
2021-05-02 16:58:08 +0800

    添加了zfh

--- src/main/java/com/minesweeper/MineSweeper/Controler/ZFH.java
+++ src/main/java/com/minesweeper/MineSweeper/Controler/ZFH.java
@@ -3,5 +3,6 @@ package com.minesweeper.MineSweeper.Controler;
 public class ZFH {
     public static void main(String[] args) {
         System.out.println("zfh");
+        System.out.println("yc YiXuan");
     }
 }
