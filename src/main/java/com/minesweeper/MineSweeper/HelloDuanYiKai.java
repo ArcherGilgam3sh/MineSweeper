6b3fada64fe48765b0677def4fa21e2b48c3fd52
YuchiYixuan <83524799+YuchiYixuan@users.noreply.github.com>
2021-05-04 14:50:40 +0800

    Create HelloDuanYiKai.java

---
+++ src/main/java/com/minesweeper/MineSweeper/HelloDuanYiKai.java
@@ -0,0 +1,7 @@
+package com.minesweeper.MineSweeper;
+
+public class HelloDuanYiKai {
+    public static void main(String[] args) {
+        System.out.println("DuanYiKai is really stupid");
+    }
+}
