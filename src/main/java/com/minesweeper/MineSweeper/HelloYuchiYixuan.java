cf328c5c552299733e28cec737d5a1c00e9a86a4
Duanyikai <83626477+Duanyikai@users.noreply.github.com>
2021-05-15 21:41:00 +0800

    Delete HelloYuchiYixuan.java

--- src/main/java/com/minesweeper/MineSweeper/HelloYuchiYixuan.java
+++
@@ -1,7 +0,0 @@
-package com.minesweeper.MineSweeper;
-
-public class HelloYuchiYixuan {
-    public static void main(String[] args) {
-        System.out.println("YuchiYixuan is really stupid.");
-    }
-}
