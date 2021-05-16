ae2c3cc87f203f8ffc1c044a26f4669f4335d5c2
ArcherGilgam3sh <83524709+ArcherGilgam3sh@users.noreply.github.com>
2021-05-16 23:42:08 +0800

    Update Save.java

--- src/main/java/com/minesweeper/MineSweeper/Save.java
+++ src/main/java/com/minesweeper/MineSweeper/Save.java
@@ -9,6 +9,21 @@ public class Save {
     public static void main(String[] args) {
 
 
+        int[][] test=new int[20][20];
+        for (int i = 0; i < 20; i++) {
+            for (int i1 = 0; i1 < 20; i1++) {
+                Random r2=new Random();
+                test[i][i1]=r2.nextInt(10);
+            }
+        }
+
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 20; j++) {
+                System.out.print(test[i][j]+" ");
+            }
+            System.out.println();
+        }
+
         String path="";
         FileSystemView fsv = FileSystemView.getFileSystemView();
 
@@ -26,6 +41,9 @@ public class Save {
             //此处path为该文件路径
         }
 
+
+
+
         int[][] test2=new int[20][20];
 
         try{
@@ -35,11 +53,14 @@ public class Save {
                     test2[i][j]=in.read();
                 }
             }
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+
 
         try{
             BufferedWriter writer=new BufferedWriter(new FileWriter("Out.txt"));
